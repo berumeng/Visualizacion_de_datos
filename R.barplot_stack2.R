5645814f463c29e07963ffe88af1bfe6b1a204bc
berumeng <76057189+berumeng@users.noreply.github.com>
2023-08-30 15:43:21 -0700

    Add files via upload

---
+++ R.barplot_stack2.R
@@ -0,0 +1,30 @@
+##Establecer la carpeta de trabajo
+setwd("/Users/berumen/Desktop/UAN/Clases/Posgrado/CBAP/2020/Análisis_visualización_datos_R/R_script/Graficas_correlacion")
+
+#Importar datos de excel (NO DEBE TENER ESPACIOS)
+Datos <- read.table("COVID_NAY_4.txt", header=TRUE)
+head(Datos)
+
+#MODELO DE REGRESION LINEAL
+regresion <- lm(Acumulados ~ Municipio, data= Datos)
+summary(regresion)
+#ECUACION RECTA ES:
+# y = 193.33 + 57.67x
+
+#GRAFICAR REGRESION LINEAL
+plot(Datos$Municipio, Datos$Acumulados, xlab="Municipio", ylab = "Acumulados")
+abline(regresion)
+
+# library
+library(dplyr)
+library(ggplot2)
+theme_set(theme_minimal())
+
+#create graphic
+ggplot(Datos, aes(x = Municipio, y = Acumulados))+
+  geom_col(aes(fill=Casos), width = 0.7)
+
+
+# Grouped
+ggplot(Datos, aes(fill=Casos, y=Acumulados, x=Municipio)) + 
+  geom_bar(position="stack", stat="identity")
